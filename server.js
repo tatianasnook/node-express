5e534a303ff9b896de1dfbe63b23a642444a40e8
tatianasnook <tatsnook@gmail.com>
2023-12-29 15:11:47 -0600

    workshop assignment. task 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
 
 app.use(express.static(__dirname + '/public'));
 
